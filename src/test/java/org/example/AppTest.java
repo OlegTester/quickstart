89d6738200908a400c93e1d99bb2e510b09c8991
Oleg <olegaleynikoff@gmail.com>
2020-12-28 19:50:17 +0300

    Commit 5

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,8 +14,12 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() { assertTrue( true ); }
+
+
+
+    @Test
+    public void testSum() {
+        assertEquals(App.s);
     }
 }
